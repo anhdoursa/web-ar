24f87fd8524b76aa93da90e2eae99ff6be25b3a0
anhdoursa <anh.do@ursa.vn>
2023-05-04 11:25:41 +0700

    .

--- src/components/scene.jsx
+++ src/components/scene.jsx
@@ -1,12 +1,30 @@
-import { Interactive } from "@react-three/xr";
-import React from "react";
+import { Interactive, useHitTest } from '@react-three/xr';
+import React, { useRef } from 'react';
 
 const Scene = () => {
+  const mesh = useRef();
+  const reticle = useRef();
+  useHitTest((hitMatrix, hit) => {
+    if (hit) {
+      reticle.current.visible = true;
+    } else {
+      reticle.current.visible = false;
+    }
+    // use hitMatrix to position any object on the real life surface
+    hitMatrix.decompose(reticle.current.position, reticle.current.quaternion, reticle.current.scale);
+
+    // console.log(hit);
+  });
+
   return (
     <Interactive>
-      <mesh>
+      <mesh ref={mesh}>
         <boxGeometry />
         <meshNormalMaterial />
+      </mesh>
+      <mesh ref={reticle} visible={false}>
+        <ringGeometry args={[0.15, 0.2, 32]} rotateX={-Math.PI / 2} />
+        <meshBasicMaterial />
       </mesh>
     </Interactive>
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
-import Scene from "@/components/scene";
-import { Canvas } from "@react-three/fiber";
-import { ARButton, Controllers, XR } from "@react-three/xr";
+import Scene from '@/components/scene';
+import { Canvas } from '@react-three/fiber';
+import { ARButton, Controllers, XR } from '@react-three/xr';
 
 export default function Home() {
   return (
     <>
       <ARButton />
       <Canvas>
-        <XR>
+        <XR referenceSpace="local">
           <Scene />
+          <Controllers />
         </XR>
-        <Controllers />
       </Canvas>
     </>
   );
